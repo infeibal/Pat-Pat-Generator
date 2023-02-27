0efa659bf92ff23d75756ac350720fb95013c305
infeibal <theliamegames@gmail.com>
2023-02-27 23:13:51 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
+import './App.css'
+import Myimage from './Components/Myimage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+  const [value, setValue] = useState("https://cdn3.iconfinder.com/data/icons/emoji-1-4/64/_angry_sad_unhappy-256.png")
+
+  let switchTougle;
+
+  const swapImg = () => {
+    if (!switchTougle) {
+      setValue("https://cdn3.iconfinder.com/data/icons/emoji-1-4/64/_angry_sad_unhappy-256.png")
+    } else {
+      setValue(switchTougle)
+    }
+  }
+  
+  return(
+    <div className='App'>
+      <div className="content">
+        <header>
+          <h1>Let Him Pat You!</h1>
+        </header>
+        <div className="conteiner">
+        <div className='main'>
+          <Myimage />
+          <div className="yourface">
+              <img src={value} alt="stock img" className='penis'/>
+            
+          </div>
+        </div>
+        
+        <div className="input-n-button">
+          <input type="text" placeholder='Write URL' onChange={event => switchTougle = event.target.value} className="inpt"/>
+          <button onClick={swapImg} className="btn">Upload</button>
+        </div>
+        </div>
+      </div>
+    </div> 
+  )
 }
 
 export default App;
---
+++ src/Components/Myimage.jsx
@@ -0,0 +1,11 @@
+import Animation from '../images/hand.gif'
+
+const Myimage = () => {
+    return(
+     
+            <img src={Animation} alt="" className="hand"/>
+       
+    )
+}
+
+export default Myimage;
